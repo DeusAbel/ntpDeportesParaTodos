4a12e8616bb4b0cfb64037964316a8cf7ee68efb
Abel <abel.harc@live.com>
2016-10-10 02:02:44 -0500

    V1

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-# ntpDeportesParaTodos
-este es un reporistorio para el curso de nuevas plataformas...
+# jadebootstrap-seed
+[JADE-Bootstrap](http://rajasegar.github.io/JADE-Bootstrap/) boilerplate project for [NodeJS](http://nodejs.org/) using [Express](http://expressjs.com) middleware
 
-no juegen dota
+## Quick Start
 
-Versines de:
+  The quickest way to get started with this boilerplate is to install the packages first and use the npm script to start the application as shown below:
 
-NPM
-NODE
-MONGODB
-EXPRESS
-ANGULAR
-BOOSTRAP
-JADE
+  Install the packages.
 
+```bash
+$ git clone https://github.com/rajasegar/jadebootstrap-seed.git
+$ cd jadebootstrap-seed
+$ npm install
+$ npm start
+```
+## Features
+
+  * Bootstrap set as the default UI framework
+  * JADE set as the default Template engine
+  * Focus on high performance
+  * Super-speed development using JADE mixins
+  
--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/xeventgo_db');
 
-var routes = require('./app_server/routes/index');
-// var users = require('./app_server/routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -24,13 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/users', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -38,23 +37,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
