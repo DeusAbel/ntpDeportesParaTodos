e90e90982d811d088e0b8fc80186db0248814e09
Abel <abel.harc@live.com>
2016-11-27 22:35:50 -0500

    correct heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 require('./app_api/models/ntpDeporteParaTodos_db');
 
 
